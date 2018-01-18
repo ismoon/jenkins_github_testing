8f099ff95cdbc04ef65f62b2c3e3511c34a87c96
ismoon <insu4341@gmail.com>
2018-01-18 21:29:37 +0900

    first commit

---
+++ src/main/java/com/ismoon/jenkinsgithubtesting/JenkinsGithubTestingApplication.java
@@ -0,0 +1,12 @@
+package com.ismoon.jenkinsgithubtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGithubTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGithubTestingApplication.class, args);
+	}
+}
