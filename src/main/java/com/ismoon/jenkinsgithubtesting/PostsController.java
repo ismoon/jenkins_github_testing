1e4a7be7b42056e82b14e89b2ebd7b93be594556
ismoon <insu4341@gmail.com>
2018-01-18 22:30:06 +0900

    Push Hook Test - Controller 수정 작업 및 show.html 파일 추가

--- src/main/java/com/ismoon/jenkinsgithubtesting/PostsController.java
+++ src/main/java/com/ismoon/jenkinsgithubtesting/PostsController.java
@@ -3,6 +3,7 @@ package com.ismoon.jenkinsgithubtesting;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,5 +17,12 @@ public class PostsController {
     public String newPost(Model model) {
         model.addAttribute("post", new Post());
         return "new";
+    }
+
+    @RequestMapping(value = "/posts", method = RequestMethod.POST)
+    public String createPost(@ModelAttribute Post post, Model model) {
+        model.addAttribute("post", post);
+
+        return "show";
     }
 }
