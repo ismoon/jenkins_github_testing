c26c27ba37d7964c04c3b4e50a3c9693ad2bbd27
ismoon <insu4341@gmail.com>
2018-01-18 22:24:04 +0900

    Push Hook Test를 위한 테스트

---
+++ src/main/java/com/ismoon/jenkinsgithubtesting/PostsController.java
@@ -0,0 +1,20 @@
+package com.ismoon.jenkinsgithubtesting;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author ismoon on 2018. 1. 18.
+ */
+@Controller
+@EnableAutoConfiguration
+public class PostsController {
+    @RequestMapping(value = "/posts/new", method = RequestMethod.GET)
+    public String newPost(Model model) {
+        model.addAttribute("post", new Post());
+        return "new";
+    }
+}
